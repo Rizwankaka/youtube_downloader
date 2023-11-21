a5b535c93d441d47d6835a21ba002a4240c26168
Rizwan Rizwan <rizwan.nazeerahmed@gmail.com>
2023-11-21 14:17:17 +0500

    changes commit

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,20 +1,29 @@
 import streamlit as st
 from pytube import YouTube
+import time
 
-def download_video(url, path):
+def download_video(url):
     yt = YouTube(url)
     stream = yt.streams.get_highest_resolution()
-    stream.download(output_path=path)
+    stream.download()
     return stream.default_filename
 
-st.title('YouTube Video Downloader')
+st.title('📥 YouTube Video Downloader by Rizwan')
 
 video_url = st.text_input('Enter the URL of the YouTube video you wish to download:')
-download_path = st.text_input('Enter the download path:')
 
 if st.button('Download'):
     try:
-        filename = download_video(video_url, download_path)
+        # Display a message and start a progress bar
+        st.info('Starting download...')
+        progress_bar = st.progress(0)
+        for percent_complete in range(100):
+            time.sleep(0.1)  # Simulate a delay (Replace with actual download progress if possible)
+            progress_bar.progress(percent_complete + 1)
+        
+        filename = download_video(video_url)
+        
         st.success(f'Video downloaded successfully: {filename}')
+        st.info('Please check your device\'s download folder.')
     except Exception as e:
         st.error(f'An error occurred: {e}')
